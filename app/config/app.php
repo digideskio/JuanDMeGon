0eecb60883556367ebf3e76f903e887b60be7557
JuanDMeGon <juandavid1231@hotmail.com>
2014-12-31 15:46:15 -0500

    Solving issue with app_key
    - Because the first time that the app is installed with composer the
    $_ENV['app_key'] value is unreachable is neccessary to use a temporal
    value (which must be replaced after installation by the app_key value in
    .env files)

--- app/config/app.php
+++ app/config/app.php
@@ -77,7 +77,9 @@ return array(
 	|
 	*/
 
-	'key' => $_ENV['app_key'],
+	'key' => getenv('app_key') ?: 'please-change-for-security',
+	// Take into account that if app_key is not stablished in a .env.*.php randomly...
+	// ...The security associated with the key will be vulnerable
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
